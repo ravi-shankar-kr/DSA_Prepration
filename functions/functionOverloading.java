345e14c1b1e9e39f9765d2d80e6bbdf6a5d08583
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-09-03 19:21:20 +0530

    initial commit

---
+++ functions/functionOverloading.java
@@ -0,0 +1,18 @@
+package functions;
+
+public class functionOverloading {
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5,3));
+        System.out.println(sum(5, 2, 6));
+
+    }
+}
