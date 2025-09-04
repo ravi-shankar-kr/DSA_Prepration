49501d39464731daad1b105506becad372be2048
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-09-04 20:11:01 +0530

    update code for n == 2

--- functions/prime.java
+++ functions/prime.java
@@ -4,6 +4,11 @@ public class prime {
     
     public static boolean  isPrime(int n){
         boolean isPrime =  true;
+        
+        if(n == 2){
+            isPrime = true;
+        }
+
 
         for(int i=2; i<= n-1; i++){
             if ( n % i == 0) {
@@ -16,7 +21,7 @@ public class prime {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(4  ));
+        System.out.println(isPrime(4 ));
     }
 }
 
