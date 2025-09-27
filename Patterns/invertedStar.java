d3e7a9af2697e7d46b54512e19029c76258170c9
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-09-27 17:39:02 +0530

    first commit

---
+++ Patterns/invertedStar.java
@@ -0,0 +1,11 @@
+package Patterns;
+public  class invertedStar {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 4-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
