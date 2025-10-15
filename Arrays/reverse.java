760e81d0de8ddb088465b5fa3f64086b5ea6c864
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-10-15 17:04:32 +0530

    first commit

---
+++ Arrays/reverse.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class reverse {
+    
+    public static void reverseArrays(int numbers[]){
+        int first = 0, last = numbers.length - 1;
+
+        while(first< last ){
+            // swap
+
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4, 6, 8, 10};
+
+        reverseArrays(numbers);
+
+        for(int i=0; i<numbers.length; i++){
+            System.out.print(numbers[i]+ " ");
+        }
+        System.out.println();
+    }
+}
