f84d18f6c986f993fa2ba5e243ecc7694537500e
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-10-15 17:19:12 +0530

    add total pairs

--- Arrays/pairsArray.java
+++ Arrays/pairsArray.java
@@ -3,13 +3,16 @@ package Arrays;
 public class pairsArray {
 
     public static void printPairs(int numbers[]) {
+        int tp = 0;
         for(int i=0; i<numbers.length; i++){
             int curr = numbers[i];    // 2,4,6,8,10
             for(int j=i+1; j<numbers.length; j++){
                 System.out.print("(" + curr + "," + numbers[j] + ") ");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total pairs: " + tp);
     }
 
     public static void main(String[] args) {
